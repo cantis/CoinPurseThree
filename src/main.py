2d9fa9fee10f08c56be84bf16488c20630082467
Evan Young <cantis@gmail.com>
2023-11-04 13:54:03 -0500

    cleaned up...

--- src/main.py
+++ src/main.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# from src.routers.characters import router as characters_router
-# from src.routers.players import router as players_router
-# from src.routers.transaction import router as transaction_router
-
 from database import Base
 
 DATABASE_URL = "sqlite:///../instance/coin_purse.db"
@@ -14,7 +10,6 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 app = FastAPI()
-
 
 # Dependency to get the database session
 def get_db():
@@ -28,28 +23,6 @@ def get_db():
 @app.on_event("startup")
 async def startup():
     Base.metadata.create_all(bind=engine)
-
-
-# app.include_router(
-#     players_router,
-#     prefix="/players",
-#     tags=["players"],
-#     responses={404: {"description": "Not found"}},
-# )
-
-# app.include_router(
-#     characters_router,
-#     prefix="/characters",
-#     tags=["characters"],
-#     responses={404: {"description": "Not found"}},
-# )
-
-# app.include_router(
-#     transaction_router,
-#     prefix="/transactions",
-#     tags=["transactions"],
-#     responses={404: {"description": "Not found"}},
-# )
 
 # App Running check
 @app.get("/")
