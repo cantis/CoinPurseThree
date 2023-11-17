2988418866622af1d85041c4cec085a686151734
Evan Young <cantis@gmail.com>
2023-11-16 20:23:43 -0600

    Updated to use RUFF formatting

--- src/main.py
+++ src/main.py
@@ -1,19 +1,22 @@
 """Coinpurse API."""
+import os
 from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from database.models import Base
 
-DATABASE_URL = "sqlite:///../instance/coin_purse.db"
+DATABASE_URL = 'sqlite:///../instance/coin_purse.db'
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 app = FastAPI()
 
+
 # Dependency to get the database session
 def get_db():
+    """Get a database session."""
     database = SessionLocal()
     try:
         yield database
@@ -21,12 +24,14 @@ def get_db():
         database.close()
 
 
-@app.on_event("startup")
+@app.on_event('startup')
 async def startup():
-    Base.metadata.create_all(bind=engine)
+    """Create the database tables, if they don't already exist."""
+    if not os.path.exists(DATABASE_URL):
+        Base.metadata.create_all(bind=engine)
 
 
-@app.get("/")
+@app.get('/')
 async def root():
     """Root endpoint for the API."""
-    return {"message": "Coinpurse is UP!"}
+    return {'message': 'Coinpurse is UP!'}
