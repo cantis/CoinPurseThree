c35c87cca021918631048b42323da1c3d1c18a4d
Evan Young <cantis@gmail.com>
2023-11-25 21:19:13 -0600

    Add configurations for Python: FastAPI and create instance folder if it doesn't exist

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
 """Coinpurse API."""
+from contextlib import asynccontextmanager
+import os
 from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from database.models import Base
 
+INSTANCE_FOLDER_PATH = "../instance"
 DATABASE_URL = "sqlite:///../instance/coin_purse.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 app = FastAPI()
+
 
 # Dependency to get the database session
 def get_db():
@@ -23,6 +27,9 @@ def get_db():
 
 @app.on_event("startup")
 async def startup():
+    # Create the instance folder if it doesn't exist
+    if not os.path.exists(INSTANCE_FOLDER_PATH):
+        os.makedirs(INSTANCE_FOLDER_PATH)
     Base.metadata.create_all(bind=engine)
 
 
