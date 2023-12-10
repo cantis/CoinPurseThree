8c260342e32e77ea68c0549ed75e768bc179d51b
Evan Young <cantis@gmail.com>
2023-12-10 00:02:11 -0600

    Refactor FastAPI metadata

--- src/main.py
+++ src/main.py
@@ -10,43 +10,33 @@ DATABASE_URL = 'sqlite:///coin_purse.db'
 
 sesh = sessionmaker(bind=create_engine(DATABASE_URL))
 
+# Create the FastAPI app and set some metadata
+# See https://fastapi.tiangolo.com/tutorial/metadata/ for notes on openapi_tags (these show up in the swagger docs)
+# Note: Markdown is supported in the description fields.
 app = FastAPI(
-    title="Coinpurse API",
-    description="API for Coinpurse, a fantasy RPG wallet app.",
-    version="0.1.0",
-    openapi_url="/api/v1/openapi.json",
-    openapi_tags=[{
-        "name": "Test",
-        "description": "Diagnostic endpoint(s)."
-    }, {
-        "name": "Players",
-        "description": "Endpoints for Player Management."
-    }, {
-        "name": "Characters",
-        "description": "Endpoints for Character Management."
-    }, {
-        "name": "Transactions",
-        "description": "Endpoints for Transactions."
-    }]
+    title='Coinpurse API',
+    contact={
+        'name': 'Evan Young',
+        'email': 'cantis@gmail.com',
+    },
+    summary='A fantasy RPG wallet api backend.',
+    description="""First attempt at a FastAPI project, a **Fantasy RPG Wallet App**, represents a characters *coinpurse* or wallet.
+      Provides a record of what they get from adventuring and how they spend it.""",
+    version='0.1.0',
+    openapi_url='/api/v1/openapi.json',
+    openapi_tags=[
+        {'name': 'Test', 'description': 'Diagnostic endpoint(s).'},
+        {'name': 'Players', 'description': 'Endpoints for Player Management.'},
+        {'name': 'Characters', 'description': 'Endpoints for Character Management.'},
+        {'name': 'Transactions', 'description': 'Endpoints for Transactions.'},
+    ],
+    license_info={
+        'name': 'MIT',
+        'url': 'https://opensource.org/licenses/MIT',
+    },
 )
 
-    # docs_url="/",
-    # redoc_url=None,
-    # openapi_tags=[{
-    #     "name": "Test",
-    #     "description": "Test endpoints."
-    # }, {
-    #     "name": "Players",
-    #     "description": "Endpoints for the Player resource."
-    # }, {
-    #     "name": "Characters",
-    #     "description": "Endpoints for the Character resource."
-    # }, {
-    #     "name": "Transactions",
-    #     "description": "Endpoints for the Transaction resource."
-    # }]
-
-
+# Include the routers
 app.include_router(players.router)
 app.include_router(characters.router)
 app.include_router(transaction.router)
@@ -62,7 +52,7 @@ def get_db() -> None:
         database.close()
 
 
-@app.get('/', tags=["Test"])
+@app.get('/', tags=['Test'])
 async def root() -> dict:
     """Root endpoint for the API."""
     return {'message': 'Coinpurse is UP!'}
