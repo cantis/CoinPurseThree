70a2483c2015a3b1f6382ec81f152b021b7b88be
Evan Young <cantis@gmail.com>
2024-03-07 08:10:16 -0600

    Ok, that works with create_all!

--- tests/test_player.py
+++ tests/test_player.py
@@ -9,7 +9,7 @@ from sqlalchemy import create_engine, StaticPool
 from sqlalchemy.orm import sessionmaker
 
 from src.main import app
-from database.models import get_db
+from database.models import get_db, Base
 
 
 LOG_PATH = path.join(path.dirname(path.abspath(__file__)), '../tests/test.log')
@@ -23,8 +23,8 @@ logging.debug('Test Initalize')
 
 client = TestClient(app)
 
-TEST_DATABASE_URL = 'sqlite:///:memory:'
-# TEST_DATABASE_URL = 'sqlite:///instance/coin_purse_temp_test.db'
+# TEST_DATABASE_URL = 'sqlite:///:memory:'
+TEST_DATABASE_URL = 'sqlite:///instance/coin_purse_temp_test.db'
 
 engine = create_engine(
     TEST_DATABASE_URL,
@@ -49,38 +49,42 @@ def override_get_db():
 app.dependency_overrides[get_db] = override_get_db
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope='module', autouse=True)
 def create_test_database():
     """Setup the database for testing."""
 
     # # Drop the database
     logging.debug('Dropping the database')
-    # file_path = TEST_DATABASE_URL.removeprefix('sqlite:///')
-    # if path.exists(file_path):
-    #     os.remove(file_path)
+    file_path = TEST_DATABASE_URL.removeprefix('sqlite:///')
+    if path.exists(file_path):
+        os.remove(file_path)
 
     # Create the database
     logging.debug('Applying migrations')
     engine = create_engine(TEST_DATABASE_URL)
-    alembic_cfg = Config()
-    with engine.begin() as connection:
-        alembic_cfg.attributes['connection'] = connection
-        alembic_cfg.set_main_option('sqlalchemy.url', TEST_DATABASE_URL)
-        alembic_cfg.set_main_option('script_location', './src/alembic')
-        try:
-            command.upgrade(alembic_cfg, 'head')
-        except Exception as e:
-            logging.error(f'Exception during migration: {e}')
-            raise e
-        yield
+    Base.metadata.create_all(engine)
+    yield
+    engine.dispose()
+    # alembic_cfg = Config()
+    # with engine.begin() as connection:
+        # alembic_cfg.attributes['connection'] = connection
+        # alembic_cfg.set_main_option('sqlalchemy.url', TEST_DATABASE_URL)
+        # alembic_cfg.set_main_option('script_location', './src/alembic')
+        # try:
+
+        #     # command.upgrade(alembic_cfg, 'head')
+        # except Exception as e:
+        #     logging.error(f'Exception during migration: {e}')
+        #     raise e
+        # yield
         # Clean up
-        engine.dispose()
+        # engine.dispose()
         # logging.debug('Dropping the database')
         # if path.exists(file_path):
         #     os.remove(file_path)
 
 
-def test_create_player(create_test_database) -> None:
+def test_create_player(create_test_database: None) -> None:
     # arrange
     data = {
         'playerName': 'test_player',
@@ -108,7 +112,7 @@ def test_create_player(create_test_database) -> None:
     assert response_data['isAdmin'] is False
 
 
-def test_get_player(create_test_database) -> None:
+def test_get_player(create_test_database: None) -> None:
     # arrange
 
     # act
